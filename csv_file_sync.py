043fc36a668bedc2c67827ef612c359464969aeb
KeksNino <87879013+KeksNino@users.noreply.github.com>
2025-09-20 00:03:18 +0200

    fix the entire code

--- csv_file_sync.py
+++ csv_file_sync.py
@@ -9,12 +9,13 @@ playlist_tracks = set()
 with open(download_folder + csv_file, newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
-        row = row[:-1]
-        print(f"{row[1]} - {row[0]}.mp3")
+        row = row[:-1]y
+        filename = f"{row[1]} - {row[0]}.mp3"
+        playlist_tracks.add(filename)
 
 # Scan download folder
 for file in os.listdir(download_folder):
     if file not in playlist_tracks:
         file_path = os.path.join(download_folder, file)
         os.remove(file_path)
-        print(f"Deleted: {file}")
+        print(f"Deleted File: {file}")
